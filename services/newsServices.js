f3db73fa7b58eedbe43abbdbfe6a66a6e626fa20
toure <faycal.toure@gmail.com>
2020-04-29 05:28:04 +0200

    fix news part 1

--- services/newsServices.js
+++ services/newsServices.js
@@ -2,11 +2,11 @@ const NewsAPI = require('newsapi');
 const newsapi = new NewsAPI('2fdd5376bdfe4c56929d6f07c5a2e4ac');
 
 let getNews = async (req, res) => {
-    console.log(req.body.requestLanguage, req.body.requestQuery, req.body.requestSource.toLowerCase());
+    console.log(req.body.requestLanguage, req.body.requestQuery, req.body.requestSource);
     newsapi.v2.topHeadlines({
-        sources: req.body.requestSource.toLowerCase(),
-        q: req.body.requestQuery.toLowerCase(),
-        language: req.body.requestLanguage.toLowerCase()
+        sources: req.body.requestSource,
+        q: req.body.requestQuery,
+        language: req.body.requestLanguage
     }).then(result => {
         res.status(200).send(result);
     });
